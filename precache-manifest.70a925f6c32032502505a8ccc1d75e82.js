84b6f60256cb351e9eed7e3c3f7a3ef5c3f307c9
CSantana23 <cesarsantana475@gmail.com>
2020-05-20 12:02:46 -0400

    Updates

---
+++ precache-manifest.70a925f6c32032502505a8ccc1d75e82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850c1e28f41fa883995d34dfd28a2d22",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e20dd8c9a8d9a0ac26f3",
+    "url": "/monsters-rolodex/static/css/main.b8d38dc7.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e20dd8c9a8d9a0ac26f3",
+    "url": "/monsters-rolodex/static/js/main.86541ec3.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
